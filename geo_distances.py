1acd8992b301f8dde0c58f8ea41ae03b6d95ecfd
Florian Reinhard <florian.reinhard@epfl.ch>
2014-05-03 13:05:56 +0200

    removed unused print

--- geo_distances.py
+++ geo_distances.py
@@ -12,11 +12,10 @@ f = open(sys.argv[1], 'r')
 for line in f:
     old = new
     new = parse_coordinate(line)
-    print(new)
     if count > 0:
         dist = distance(old[0], old[1], new[0], new[1])
         total += dist
-        print(line.rstrip() + "\t" + str(dist) + " nm")
+        print(line.rstrip() + "\t\t" + str(dist) + " nm")
     else:
         print(line.rstrip())
 
